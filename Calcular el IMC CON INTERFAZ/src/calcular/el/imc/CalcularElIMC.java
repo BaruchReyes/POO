70017e7b6d404f68c97e4aa2fb3f4cd14009310a
Baruch Reyes <62493850+BaruchReyes@users.noreply.github.com>
2020-06-18 02:34:44 -0500

    CALCULADORA DE IMC CON VISTA

---
+++ Calcular el IMC CON INTERFAZ/src/calcular/el/imc/CalcularElIMC.java
@@ -0,0 +1,50 @@
+
+package calcular.el.imc;
+
+  
+public class CalcularElIMC {
+
+   //Creado por Baruch Reyes con aclaraciones de Raul 
+    public static void main(String[] args) {
+
+   
+  
+    
+     vista_de_usuario e1 =new  vista_de_usuario();
+
+     e1.setVisible(true);
+     
+   /*  String nombre;
+    int edad;
+     String sexo;
+     float peso;
+     float  altura;
+        
+        /*System.out.println ("Hola ingresa tu nombre");
+         
+        nombre= entrada.nextLine();
+        System.out.println ("Ingresa tu edad");
+                edad= entrada.nextInt();
+                 System.out.println (" Ingresa tu sexo  (FEMENINO,MASCULINO)");
+              sexo=entrada.nextLine();
+              sexo=entrada.nextLine();
+                 sexo=entrada.nextLine();
+        System.out.println (" Ingresa tu peso(en KG)");
+        peso= entrada.nextFloat();
+        
+        System.out.println (" Ingresa tu altura(en MT)");
+        altura =entrada.nextFloat();
+        
+    
+      e.setNombre(nombre);
+      e.setEdad(edad);
+      e.setSexo(sexo);
+      e.setPeso(peso);
+      e.setAltura(altura);
+      
+      e.operaciones();*/
+           
+        
+    }
+    
+}
---
+++ Calcular el IMC CON INTERFAZ/src/calcular/el/imc/OPERACIONES.java
@@ -0,0 +1,35 @@
+
+package calcular.el.imc;
+
+public class OPERACIONES extends PERSONA {
+
+    private static final long serialVersionUID = 1L;
+  
+  /*public void operaciones(){
+   
+        
+        //OPERACIONES
+     altura = altura* altura;
+    //OPERACION FINAL
+    peso =  peso / altura;
+    
+    
+        System.out.println("Hola\n"+nombre);
+         System.out.println("Tu edad es:"+edad);
+         System.out.println("Tu sexo es:"+sexo);
+          System.out.println("Tu masa corporal es:"+peso);*/
+          
+          
+          
+
+        } 
+     //}
+    
+    
+     
+        
+    
+    
+    
+    
+
